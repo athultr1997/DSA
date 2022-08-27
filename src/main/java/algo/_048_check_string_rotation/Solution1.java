ea40111e6e4607e9b2b9491634a91842aad8f352
athultr <athul.tr@pickrr.com>
2022-08-27 16:06:18 +0530

    string rotation

---
+++ src/main/java/algo/_048_check_string_rotation/Solution1.java
@@ -0,0 +1,10 @@
+package algo._048_check_string_rotation;
+
+public class Solution1 {
+
+  public boolean checkRotation(String str1, String str2) {
+    String concatenatedString = str2 + str2;
+    return concatenatedString.indexOf(str1) != -1;
+  }
+
+}
